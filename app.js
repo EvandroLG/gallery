a7d02ec532b6910ae9298e31c23b67b9ad6b5bb8
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-06-23 18:22:59 +0100

    add port in config.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require("mongoose");
 
-const { db, dist } = require('./server/config');
+const { db, dist, port } = require('./server/config');
 const api = require('./server/routes/api');
 
 mongoose.connect(db, {
@@ -10,7 +10,6 @@ mongoose.connect(db, {
 });
 
 const app = express();
-const port = 3000;
 
 app.use(express.static(path.join(__dirname, dist)));
 
--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   db: "mongodb://localhost:27017/gallery",
+  port: 3000,
   dist: "dist",
   uploads: "uploads"
 };
