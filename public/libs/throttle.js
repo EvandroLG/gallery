af793cdd1eeb625774af9c226017108e88dbe15c
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-06-23 18:23:13 +0100

    create throttle function

---
+++ public/libs/throttle.js
@@ -0,0 +1,14 @@
+export default (callback, wait) => {
+  let wasCalled = false; 
+
+  return () => {
+    if (!wasCalled) {
+      wasCalled = true;
+
+      setTimeout(() => {
+        callback();
+        wasCalled = false;
+      }, wait);
+    }
+  };
+};
