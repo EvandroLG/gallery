2839d6c9b80dee96147125ef2971ad1dce1e19d2
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-07-13 11:23:45 +0100

    improve user model, adding password validation method and pre save logic

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = new mongoose.Schema({
   username: {
     required: true,
     unique: true,
     type: String,
+    trim: true,
   },
 
   email: {
     required: true,
     unique: true,
     type: String,
+    trim: true,
   },
 
   password: {
@@ -20,5 +23,34 @@ const User = new mongoose.Schema({
 }, {
   timestamp: true,
 });
+
+User.pre('save', (next) => {
+  if (this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+    next();
+  });
+});
+
+User.methods.checkPassword = () => {
+  const passwordHash = this.password;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(same);
+    });
+  });
+};
 
 module.exports = mongoose.model('User', User);
