f29b3aeab89f71f742e95350a1aa47798b3456b9
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-02-15 03:19:56 +0100

    add fadeIn animation

--- client/src/hooks/useProgressiveImage.ts
+++ client/src/hooks/useProgressiveImage.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const styles = `
   width: 100%;
@@ -7,9 +7,20 @@ const styles = `
   border-radius: 5px;
 `;
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
 const Photo = styled.img`
   ${styles}
   object-fit: cover;
+  animation: ${fadeIn} 1s;
 `;
 
 const Fallback = styled.div`
