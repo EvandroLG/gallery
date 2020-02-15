f59afaac19ad8bb06a0d15998cfc2d45bbf3f1ed
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-02-15 10:40:46 +0100

    use ThemeProvider component from styled-component

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+import theme from './configs/theme';
 import App from './components/App';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
