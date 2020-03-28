2bb17239c9957e82406215446223bdf6191122fb
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-03-28 19:23:41 +0100

    update cache strategy

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,27 +1,47 @@
 const version = 1;
 const cacheName = `gallery-v${version}`;
 
-const router = async req => {
+const request = async ({ url, headers }) => {
+  const fetchOptions = {
+    method: 'GET',
+    credentials: 'omit',
+    headers: headers,
+    cache: 'no-store',
+  };
+
+  try {
+    const response = await fetch(url, fetchOptions);
+    const cache = await caches.open(cacheName);
+    await cache.put(url, response.clone());
+
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getFromCache = async url => {
   const cache = await caches.open(cacheName);
-  const cached = await cache.match(req.url);
+  const cached = await cache.match(url);
 
   if (cached) {
     return cached.clone();
   }
+};
 
-  const url = new URL(req.url);
+const router = async req => {
+  if (req.method !== 'GET') {
+    return;
+  }
 
-  const fetchOptions = {
-    method: 'GET',
-    credentials: 'omit',
-    headers: req.headers,
-    cache: 'no-store',
-  };
+  if (req.url.includes('/api/')) {
+    const response = await request(req);
+    return response ? response : getFromCache(req.url);
+  }
 
-  const response = await fetch(url, fetchOptions);
-  await cache.put(req.url, response.clone());
+  const cached = await getFromCache(req.url);
 
-  return response;
+  return cached ? cached : request(req);
 };
 
 self.addEventListener('install', e => {
@@ -40,7 +60,7 @@ self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
   const { request } = e;
 
-  if (!request.url.includes('http') || request.method !== 'GET') {
+  if (!request.url.includes('http')) {
     return;
   }
 
